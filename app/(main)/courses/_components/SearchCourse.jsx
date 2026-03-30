fe718792eaa1ecdfa6373800fe5b959eecef3f35
Ferdous Ahmed <himibaba10@gmail.com>
2026-03-30 22:08:18 +0600

    Fixed styling of search term field in courses page

--- app/(main)/courses/_components/SearchCourse.jsx
+++ app/(main)/courses/_components/SearchCourse.jsx
@@ -36,14 +36,8 @@ const SearchCourse = () => {
 
   return (
     <div className='relative h-10 max-lg:w-full'>
-      <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground z-10 h-4 w-4' />
-      <Input
-        type='text'
-        placeholder='Search courses...'
-        value={inputValue}
-        onChange={handleSearch}
-        className='pl-8 pr-3 py-2 text-sm'
-      />
+      <Search className='absolute left-2 top-[45%] transform -translate-y-1/2 text-muted-foreground z-10 h-4 w-4' />
+      <Input type='text' placeholder='Search courses...' value={inputValue} onChange={handleSearch} className='pl-8 pr-3 py-2 text-sm' />
     </div>
   );
 };
